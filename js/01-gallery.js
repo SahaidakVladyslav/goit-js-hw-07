df783c0782c604fe26c517339f5d472692c06d0a
Vladyslav Sahaidak <svg.231@gmail.com>
2023-07-24 22:11:21 +0200

    step 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems.length)
+const markup = galleryItems.map(({ original, preview, description }) =>
+    `<li>
+    <a href="${original}">
+    <img url="${preview}" alt="${description}" width="360px" height="200px">
+    </a>
+    </li>`).join("")
+console.log(markup)
+const body = document.querySelector(`body`)
+body.insertAdjacentHTML("afterbegin", markup)
+
+
