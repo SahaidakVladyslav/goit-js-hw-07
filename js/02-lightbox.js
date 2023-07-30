91a73520d54abf00ad270cbe51c6217af50ce39b
Vladyslav Sahaidak <svg.231@gmail.com>
2023-07-31 01:02:46 +0200

    I think i did it right -_-

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+console.log(lightbox);
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const markup = galleryItems.map(({ original, preview, description }) =>
+    ` <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+</li>`).join('');
+
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+
+const modalOpenPhoto = (event) => {
+    event.preventDefault();
+
+    let gallery = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionDelay: 250,
+        fadeSpeed: 250,
+        captionSelector: "img",
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+    });
+    gallery.on('show.simplelightbox');
+};
+
+
+galleryEl.addEventListener('click', (event) => {
+    modalOpenPhoto(event)
+});
+
+
+
