a5da08ada2850ea713fe1e028da79e0fc0f5b7e7
Vladyslav Sahaidak <svg.231@gmail.com>
2023-08-02 07:31:56 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -17,36 +17,25 @@ const markup = galleryItems.map(({ original, preview, description }) =>
 
 gallery.insertAdjacentHTML('beforeend', markup);
 
-const modalCloseEsc = (element) => {
-  if (typeof element !== "undefined") {
-    document.addEventListener('keydown', function (event) {
-      const key = event.key;
-      if (key === "Escape") {
-        return element.close();
-      }
-    });
-  }
-};
 
 const modalOpenPhoto = (event) => {
   event.preventDefault();
   if (event.target.classList.contains('gallery__image')) {
     const clickedImageAlt = event.target.getAttribute('alt');
     const clickedImageSrc = event.target.dataset.source;
-    const instance = basicLightbox.create(`<img src="${clickedImageSrc}" alt="${clickedImageAlt}"/>`);
-    instance.show();
-    modalCloseEsc(instance);
-  } else {
-    const pindsvin = basicLightbox.create(`<img src="./css/enPindcvin.jpg"/>`);
-    pindsvin.show();
-    modalCloseEsc(pindsvin);
+    const instance = basicLightbox.create(`<img src="${clickedImageSrc}" alt="${clickedImageAlt}"/>`, {
+      onShow: (instance) => { },
+    });
+    instance.show(() => console.log('lightbox now visible'));
   }
+  return;
 };
 
 
 gallery.addEventListener('click', (event) => {
   modalOpenPhoto(event)
 });
+
 
 
 
